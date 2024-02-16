5e8777b0e0ec97facf78ae9b53e23fd56cb74425
Alex Volkov <a.volkov@3l.ru>
2024-02-16 22:01:25 +0300

    refactor: update file structure

--- src/components/ui/button-feedback.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <button class="button-bordered" title="Открыть форму обратной связи" type="button">
-    <span>Оставить заявку</span>
-  </button>
-</template>
-
-<script lang="ts">
-export default {}
-</script>
