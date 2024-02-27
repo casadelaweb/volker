a888b68e4261039660de5653a970220121825ef7
Alex Volkov <a.volkov@3l.ru>
2024-02-27 19:33:20 +0300

    fix: minor bugfixes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import 'src/styles/main.scss'
 import { createApp } from 'vue'
 import router from 'src/router/router'
+import { createPinia } from 'pinia'
 import App from 'src/views/App.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = createApp(App)
+  const store = createPinia()
+
   app.use(router)
+  app.use(store)
+
   app.mount('#app')
 }, { passive: true, once: true, })
 
